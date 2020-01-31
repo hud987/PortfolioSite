10484f89f06909c83accfd336735c522fbeaaac4
Michael Hudachek <hud987@gmail.com>
2020-01-31 01:42:18 -0500

    changed favicon

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'reactstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 
 //128 x 128
 import bikesafety from '../images/bikesafety.png'
@@ -13,11 +13,11 @@ export default class Homepage extends Component {
         <div style={{display: 'flex', justifyContent: 'center'}}>
           <h1>**PIC HERE**</h1>
         </div>
-        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <div style={{color: 'black', display: 'flex', justifyContent: 'center'}}>
           <h1 style={{fontSize: 60}}>Michael Hudachek</h1>
         </div>
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-          Email | GitHub | LinkedIn
+        <div style={{color: 'black',display: 'flex', justifyContent: 'center'}}>
+        <a href='mailto:mhudach@purdue.edu' style={{color: 'black'}}> Email</a>&nbsp;|&nbsp;<a href='https://github.com/hud987/' style={{color: 'black'}}>GitHub</a>&nbsp;|&nbsp;<a href='https://www.linkedin.com/in/michael-hudachek-4bb67812a/' style={{color: 'black'}}>LinkedIn</a>
         </div>
         <Row style={{paddingTop: 50, height: 90, justifyContent: 'center'}}>
           <Col sm={5} style={{
@@ -31,8 +31,10 @@ export default class Homepage extends Component {
             <div>
             <div style={{display: 'flex', justifyContent: 'center'}} ><img src={bikesafety} alt="bike safety"/></div>
             <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}><h2 style={{color: 'black'}}>Bike Safety App</h2></div>
-            <div style={{paddingTop: 20}}><h6 style={{color: 'black'}}>Mobile application to warn drivers of nearby bicyclists</h6></div>
-            <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}><Button style={{color: 'black'}}>GitHub</Button><div></div></div>
+            <div style={{paddingTop: 20}}><h6 style={{color: 'black'}}>Mobile app to warn drivers of nearby bicyclists</h6></div>
+            <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}>
+            <Button onClick={() => {window.location='https://github.com/hud987/BikeSafetyApp'}} variant="dark" style={{backgroundColor: '#212529'}}>GitHub</Button>
+            </div>
             </div>
           </Col>
           <Col sm={5} style={{
@@ -46,11 +48,19 @@ export default class Homepage extends Component {
             <div>
             <div style={{display: 'flex', justifyContent: 'center'}} ><img src={buglogger} alt="bike safety"/></div>
             <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}><h2 style={{color: 'black'}}>Bug Logger</h2></div>
-            <div style={{paddingTop: 20}}><h6 style={{color: 'black'}}>Mobile application to warn drivers of nearby bicyclists</h6></div>
+            <div style={{paddingTop: 20}}><h6 style={{color: 'black'}}>MERN web app to track project bugs</h6></div>
             <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}>
-              <Button style={{color: 'black'}}>GitHub</Button>
-              <Button style={{color: 'black'}}>Check It Out</Button>
-            </div>
+              <Row>
+              <Col></Col>
+              <Col>
+              <Button onClick={() => {window.location='https://github.com/hud987/BugLogger'}} variant="dark" style={{backgroundColor: '#212529'}}>GitHub</Button>
+              </Col>
+              <Col>
+              <Button onClick={() => {window.location='https://morning-mountain-28085.herokuapp.com/'}} variant="dark" style={{backgroundColor: '#212529'}}>Visit</Button>
+              </Col>
+              <Col></Col>
+              </Row>
+              </div>
             </div>
           </Col>
         </Row>
