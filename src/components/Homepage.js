80fe53a7ff3755cf1a5183f7ba0c1fbde08c32d4
Michael Hudachek <hud987@gmail.com>
2020-09-29 00:46:30 -0400

    added new job

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,6 +6,7 @@ import bikesafety from '../images/bikesafety.png'
 import buglogger from '../images/buglogger.png'
 import profile from '../images/selfie.png'
 import sdn from '../images/sdn.png'
+import echelon from '../images/echelon.png'
 
 export default class Homepage extends Component {
   render() {
@@ -33,6 +34,31 @@ export default class Homepage extends Component {
             backgroundColor: 'rgba(255, 255, 255, .1)'}}
           >
             <div>
+            <div style={{display: 'flex', justifyContent: 'center', paddingTop: 40, paddingBottom: 40}} ><img width={"50%"} src={echelon} alt="Echelon Consulting"/></div>
+            <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}><h2 style={{color: 'black'}}>Echelon Consulting</h2></div>
+            <div style={{paddingTop: 10}}><h6 style={{color: 'black'}}>Currently Employed</h6></div>
+            <div style={{paddingTop: 0}}><h6 style={{color: 'black'}}>Devloping automation software to reduce support costs</h6></div>
+            <div style={{paddingTop: 10, display: 'flex', justifyContent: 'center'}}>
+              <Row>
+              <Col>
+              </Col>
+              <Col>
+              </Col>
+              </Row>
+              
+              </div>
+            </div>
+          </Col>
+          <Col sm={5} style={{
+            margin: 10,
+            padding: 20,
+            borderRadius:3, 
+            marginRight: 20, 
+            display: 'flex', 
+            justifyContent: 'center',
+            backgroundColor: 'rgba(255, 255, 255, .1)'}}
+          >
+            <div>
             <div style={{display: 'flex', justifyContent: 'center'}} ><img src={bikesafety} alt="bike safety"/></div>
             <div style={{paddingTop: 20, display: 'flex', justifyContent: 'center'}}><h2 style={{color: 'black'}}>Bike Safety App</h2></div>
             <div style={{paddingTop: 10}}><h6 style={{color: 'black'}}>Mobile app to warn drivers of nearby bicyclists</h6></div>
@@ -42,6 +68,11 @@ export default class Homepage extends Component {
             </div>
             </div>
           </Col>
+          <Col></Col>
+        </Row>
+
+        <Row style={{paddingTop: 10,display: 'flex', textAlign: 'center'}}>
+          <Col ></Col>
           <Col sm={5} style={{
             margin: 10,
             padding: 20,
@@ -69,11 +100,6 @@ export default class Homepage extends Component {
               </div>
             </div>
           </Col>
-          <Col></Col>
-        </Row>
-
-        <Row style={{paddingTop: 10,display: 'flex', textAlign: 'center'}}>
-          <Col ></Col>
           <Col sm={5} style={{
             margin: 10,
             padding: 20,
@@ -99,16 +125,6 @@ export default class Homepage extends Component {
               </Row>
             </div>
             </div>
-          </Col>
-          <Col sm={5} style={{
-            margin: 10,
-            padding: 20,
-            borderRadius:3, 
-            marginRight: 20, 
-            display: 'flex', 
-            justifyContent: 'center',
-            backgroundColor: 'rgba(255, 255, 255, .0)'}}
-          >
           </Col>
           <Col></Col>
         </Row>
